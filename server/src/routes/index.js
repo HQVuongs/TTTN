01cef78330696deffc656cba7c1bea7fe6c3f5ef
HQVuongs <118650715+HQVuongs@users.noreply.github.com>
2024-08-18 00:09:01 +0700

    Delete server directory

--- server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const UserRouter = require('./UserRouter')
-const ProductRouter = require('./ProductRouter')
-
-const routes = (app) => {
-    app.use('/api/product', ProductRouter)
-    app.use('/api/user', UserRouter)
-}
-
-module.exports = routes
