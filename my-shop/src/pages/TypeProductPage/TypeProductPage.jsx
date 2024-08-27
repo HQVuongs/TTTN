d86155fa1467337d2fa1c9b406c41397df15cd5e
vuong <2151120058@ut.edu.vn>
2024-08-27 23:35:38 +0700

    update 27/8

--- my-shop/src/pages/TypeProductPage/TypeProductPage.jsx
+++ my-shop/src/pages/TypeProductPage/TypeProductPage.jsx
@@ -1,33 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavbarComponent from '../../components/NavbarComponent/NavbarComponent'
 import CardComponent from '../../components/CardComponent/CardComponent'
 import { Col, Pagination, Row } from 'antd'
 import { WrapperNavbar, WrapperProducts } from './style'
+import { useLocation } from 'react-router-dom'
+import * as ProductService from "../../services/ProductService";
+import Loading from '../../components/LoadingComponent/Loading'
+import { useSelector } from 'react-redux'
+import { useDebounce } from '../../hooks/useDebounce'
 
 const TypeProductPage = () => {
-  const onChange =() => {}
+  const searchProduct = useSelector((state) => state?.product?.search)
+  const {state} = useLocation();
+  const [products, setProducts] = useState([]);
+  const [pending, setPending] = useState(false);
+  const searchDebounce = useDebounce(searchProduct, 500)
+  const [panigate, setPanigate] = useState({
+    page: 1,
+    limit: 8,
+    total: 0,
+  })
+  const fetchProductType = async (type, page, limit) => {
+    setPending(true)
+    const res = await ProductService.getProductType(type, page, limit)
+    if(res?.status === "OK") {
+
+      setPending(false)
+      setProducts(res?.data)
+
+      setPanigate({...panigate, total: res?.total})
+    }else {
+      setPending(false)
+
+    }
+  }
+  useEffect(() => {
+    if(state ) {
+      fetchProductType(state, panigate.page, panigate.limit)
+
+    }
+  }, [state,panigate.page, panigate.limit])
+  const onChange = (current, pageSize) => {
+    setPanigate({...panigate, page: current - 1, limit: pageSize}) 
+}
   return (
-    <div style={{ width: '100%', background: '#efefef'}}>
-    <div style={{ width:'1270px', margin: '0 auto' }}>
-      <Row style={{ flexWrap: 'nowrap', paddingTop: '10px' }}>
+    <Loading isPending={pending}>
+    <div style={{ width: '100%', background: '#efefef', height: "calc(100vh - 64px)"}}>
+    <div style={{ width:'1270px', margin: '0 auto', height: "100%" }}>
+      <Row style={{ flexWrap: 'nowrap', paddingTop: '10px', height: "100%"  }}>
           <WrapperNavbar span={4}>
               <NavbarComponent />
           </WrapperNavbar>
-          <Col span={20}>
-            <WrapperProducts span={20}>
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
-                <CardComponent />
+          <Col span={20} style={{ display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+            <WrapperProducts>
+              {products?.filter((pro) => {
+                if(searchDebounce === "") {
+                  return pro
+                }else if(pro?.name?.toLowerCase()?.includes(searchDebounce?.toLowerCase())) {
+                  return pro
+                }
+              })?.map((product) => {
+                return (
+                  <CardComponent
+                  key={product._id}
+                  countInStock={product.countInStock}
+                  description={product.description}
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                  rating={product.rating}
+                  type={product.type}
+                  selled={product.selled}
+                  discount={product.discount}
+                  id={product._id}
+                />
+
+                )
+              })}
             </WrapperProducts>
-            <Pagination defaultCurrent={2} total={100} onChange={onChange} style={{ display: 'flex', justifyContent: 'center', marginTop: '10px'}} />
+            <Pagination defaultCurrent={panigate.page} total={panigate?.total} pageSize={panigate?.limit} onChange={onChange} style={{ display: 'flex', justifyContent: 'center', marginTop: '10px'}} />
           </Col>
       </Row>
     </div>
     </div>
+    </Loading>
   )
 }
 
--- my-shop/src/services/ProductService.js
+++ my-shop/src/services/ProductService.js
@@ -51,3 +51,8 @@ export const getAllTypeProduct = async () => {
     const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all-type`)
     return res.data
 }
+
+export const getProductType = async (type, page, limit) => {
+      const  res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all?filter=type&filter=${type}&limit=${limit}&page=${page}`)
+    return res.data
+}
